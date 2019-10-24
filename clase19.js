2eb6ed69d066ffcb65612bba3ebda8f95b7bcdea
sebastian <sebaxtian098@hotmail.com>
2019-10-24 17:48:02 -0500

    transformar un array

---
+++ clase19.js
@@ -0,0 +1,43 @@
+var sebastian = {
+    nombre: 'sebastian',
+    apellido: 'calapsu',
+    altura: 1.73
+}
+var diana = {
+    nombre: 'diana',
+    apellido: 'castro',
+    altura: 1.70
+}
+var santiago = {
+    nombre: 'santiago',
+    apellido: 'martinez',
+    altura: 1.80
+}
+var catalina = {
+    nombre: 'catalina',
+    apellido: 'vuitriago',
+    altura: 1.69
+}
+var mariana = {
+    nombre: 'mariana',
+    apellido: 'ospina',
+    altura: 1.71
+}
+
+const esAlta = ({altura}) => altura >= 1.70
+const esBaja = ({altura}) => altura < 1.70
+
+
+var personas = [sebastian, diana, santiago, catalina, mariana]
+
+var personas_altas = personas.filter(esAlta)
+var personas_bajas = personas.filter(esBaja)
+
+const pasarAlturaCms = persona => ({
+        ...persona,
+        persona: persona.altura * 100
+    })
+
+var personasCms = personas.map(pasarAlturaCms)
+
+console.log(personasCms)
