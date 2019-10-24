12e1156b646bc5d1ed2f1dd1ebe77df6696f0185
sebastian <sebaxtian098@hotmail.com>
2019-10-23 20:58:08 -0500

    switc

---
+++ clase16.js
@@ -0,0 +1,26 @@
+ var signo = prompt('¿cual es tu signo?')
+
+switch (signo){
+    case 'acuario':
+         console.log('intenta a consentrate en el estudio y no en el amor')
+    break
+    case 'leo':
+        console.log('mejorar seguir para adelante')
+    break
+    case 'tauro':
+        console.log('corre donde nadie te alcance ')
+    break
+    case 'picis':
+        console.log('buela lejos')
+    break
+    case 'cancer':
+         console.log('que nadie te detenga')
+    break
+
+    default:
+        console.log('lo siento pero no puedo encontar tu signo ')
+    break
+        
+
+
+}
