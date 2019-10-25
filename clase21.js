0322f61ddac1c03ca00b8b75c3e63b1c598e1a6a
sebastian <sebaxtian098@hotmail.com>
2019-10-24 19:23:33 -0500

    soy alto

--- clase21.js
+++ clase21.js
@@ -1,12 +1,22 @@
-function persona(nombre, apellido){
+function persona(nombre, apellido, altura){
     this.nombre = nombre
     this.apellido = apellido
+    this.altura = altura
 }
 
 persona.prototype.saludar = function (){
     console.log(`hola me llamo ${this.nombre} ${this.apellido}`)
 }
 
-var sebastian = new persona('sebastian','calapsu')
-var erika = new persona('erika','calapsu')
-var santiago = new persona ('santiago','medez')
+var sebastian = new persona('sebastian','calapsu', 1.75)
+var erika = new persona('erika','calapsu', 1.80)
+var santiago = new persona ('santiago','medez', 1.65)
+
+persona.prototype.soyalto = function() {
+    if (this.altura > 1.70){
+        console.log(`${this.nombre} es alto`)
+    }else{
+        console.log(`${this.nombre} es bajo`)
+    }
+    
+}
