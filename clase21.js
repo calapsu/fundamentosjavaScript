d041f80df04372b614cc86ac3163cec59ff90ac9
sebastian <sebaxtian098@hotmail.com>
2019-10-24 20:34:48 -0500

    class en javaScript

--- clase21.js
+++ clase21.js
@@ -1,22 +1,28 @@
-function persona(nombre, apellido, altura){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura = altura
-}
-
-persona.prototype.saludar = function (){
-    console.log(`hola me llamo ${this.nombre} ${this.apellido}`)
-}
-
-var sebastian = new persona('sebastian','calapsu', 1.75)
-var erika = new persona('erika','calapsu', 1.80)
-var santiago = new persona ('santiago','medez', 1.65)
-
-persona.prototype.soyalto = function() {
-    if (this.altura > 1.70){
-        console.log(`${this.nombre} es alto`)
-    }else{
-        console.log(`${this.nombre} es bajo`)
+class persona {
+    constructor (nombre, apellido, altura){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.altura = altura
     }
-    
+
+    saludar(){
+        console.log(`hola me llamo ${this.nombre} ${this.apellido}`) 
+    }
+
+    soyalto(){
+        return this.altura > 1.8
+    }
 }
+
+class desarrollador extends persona {
+    constructor (nombre,apellido,altura) {
+        super(nombre,apellido,altura)
+    }
+
+    saludar() {
+        console.log(`hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+    }
+}
+
+
+
